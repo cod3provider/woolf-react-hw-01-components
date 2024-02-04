0ff49486f3a0a93ef64283db9484053d3e932477
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-04 17:00:47 +0100

    add Statistics component

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,18 @@
+import s from './Statistics.module.css';
+
+const Statistics = ({title, stats}) => {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+
+      <ul className={s.statList}>
+        {stats.map(({id, label, percentage}) => <li className={s.item} key={id}>
+          <span className={s.label}>{label}</span>
+          <span className={s.percentage}>{percentage}</span>
+        </li>)}
+      </ul>
+    </section>
+  )
+}
+
+export default Statistics;
