4087e964a98a39f8f77f41e20bc598deb27e0a92
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-04 22:20:03 +0100

    add ternary operator for className of status

--- src/components/FriendList/FriendsItem/FriendsItem.jsx
+++ src/components/FriendList/FriendsItem/FriendsItem.jsx
@@ -1,12 +1,9 @@
 import s from './FriendsItem.module.css';
 
 const FriendsItem = ({ friends }) => {
-  const classNames = ['status', friends.status];
-  friends.status ? classNames.push('offline') : classNames.push('online');
-
   return (
-    friends.map(({ id, status, avatar, name }) => <li className={s.item} key={id}>
-      <span className={s.status}></span>
+    friends.map(({ id, isOnline, avatar, name }) => <li className={s.item} key={id}>
+      <span className={isOnline ? s.online : s.offline}></span>
       <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={s.name}>{name}</p>
     </li>)
