0f8d54c807bb0a389f92f84201e9122c67c26c83
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-04 22:49:23 +0100

    add TransactionHistory component to App + fix styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,23 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         gap: '50px',
         alignItems: 'center',
+        padding: '30px',
         fontSize: 40,
         color: '#010101'
       }}
@@ -29,6 +31,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
